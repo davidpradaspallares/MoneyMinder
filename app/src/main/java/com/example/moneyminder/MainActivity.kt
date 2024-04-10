79414db6c55b80defd6b9643561c13177974740b
David Pradas Pallares <david28122000@gmail.com>
2024-04-10 12:39:15 +0200

    MEJORAMOS APARIENCIA NUEVO GASTO Mejoramos la apariencia de la ventana "NuevoGasto"

--- app/src/main/java/com/example/moneyminder/MainActivity.kt
+++ app/src/main/java/com/example/moneyminder/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.moneyminder
 
 import android.content.Intent
-import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
